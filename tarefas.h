20f9f812b5167e160bf5b1cf28bd7a51cb16dd5d
PEDRO DOS SANTOS CONCEIÇÃO <151598422+Pedro-S-Conceicao@users.noreply.github.com>
2024-05-22 15:23:35 -0300

    Delete tarefas.h

--- tarefas.h
+++
@@ -1,9 +0,0 @@
-float somarMatriz(int ordMatriz, int *matrizA, int *matrizB, int *matrizD);
-
-float reduzirMatriz(int ordMatriz, int *matrizE);
-
-void preencherMatriz(int ordMatriz, int *matriz, char *arqDat);
-
-float multiplicarMatriz(int ordMatriz, int *matrizC, int *matrizD, int *matrizE);
-
-void escreverMatriz(int ordMatriz, int *matriz, char *arqDat);
